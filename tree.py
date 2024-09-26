90cb0c61c5342e80963e6f8f035137fc60f629b4
OumaBon <mugahboniface@gmail.com>
2024-09-26 17:13:38 +0300

    tree data structures

---
+++ tree.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+        
+        
+    def add_child(self, child_node):
+        self.children.append(child_node)
+    
+    def 
